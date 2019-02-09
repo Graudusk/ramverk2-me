280373524b6a1a73b9281cf70df47f0b6d4a2132
Eric Johansson <johanssoneric5@gmail.com>
2019-02-09 16:16:38 +0100

    Refactoring models/reports.js for smaller functions

--- models/reports.js
+++ models/reports.js
@@ -1,52 +1,47 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/texts.sqlite');
 
-module.exports = (function () {
-    function getReport(res, reportTitle) {
-        db.get("SELECT * FROM reports WHERE title = ?",
-            reportTitle,
-            (err, row) => {
-                if (err) {
-                    return res.status(500).json({
-                        errors: {
-                            status: 500,
-                            source: "/" + reportTitle,
-                            title: "Database error",
-                            detail: err.message
-                        }
-                    });
-                } else {
-                    // console.log(row.data);
-                    // console.log(JSON.parse(row.data));
-                    // res.json( { data: row } );
-                    res.json( JSON.parse(row.data) );
-                }
-            });
-    }
-
-    function addReport(res, body) {
-        db.run("INSERT INTO reports (title, data)" +
-            " VALUES (?, ?)",
-        body.title,
-        body.data,
-        (err) => {
+function getReport(res, reportTitle) {
+    db.get("SELECT * FROM reports WHERE title = ?",
+        reportTitle,
+        (err, row) => {
             if (err) {
                 return res.status(500).json({
                     errors: {
                         status: 500,
-                        source: "/report",
+                        source: "/" + reportTitle,
                         title: "Database error",
                         detail: err.message
                     }
                 });
+            } else {
+                res.json( JSON.parse(row.data) );
             }
-
-            res.status(201).json({ data: body });
         });
-    }
+}
 
-    return {
-        addReport: addReport,
-        getReport: getReport
-    };
-}());
+function addReport(res, body) {
+    db.run("INSERT INTO reports (title, data)" +
+        " VALUES (?, ?)",
+    body.title,
+    body.data,
+    (err) => {
+        if (err) {
+            return res.status(500).json({
+                errors: {
+                    status: 500,
+                    source: "/report",
+                    title: "Database error",
+                    detail: err.message
+                }
+            });
+        }
+
+        res.status(201).json({ data: body });
+    });
+}
+
+module.exports = {
+    addReport: addReport,
+    getReport: getReport
+};
