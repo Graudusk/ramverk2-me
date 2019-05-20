fa8939ed3c3ad5b52f6d8ea76539fe2c5271b0e3
Eric Johansson <johanssoneric5@gmail.com>
2019-05-20 09:24:20 +0200

    editing app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
-const index = require('./routes/index');
-const reports = require('./routes/reports');
-const register = require('./routes/register');
-const login = require('./routes/login');
-const chat = require('./routes/chat');
+const index = require("./routes/index");
+const reports = require("./routes/reports");
+const register = require("./routes/register");
+const login = require("./routes/login");
+const chat = require("./routes/chat");
 const port = 1337;
-const dsn =  process.env.DBWEBB_DSN || "mongodb://localhost:27017/chat";
+const dsn = process.env.DBWEBB_DSN || "mongodb://localhost:27017/chat";
 const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV !== 'test') {
-    app.use(morgan('combined'));
+if (process.env.NODE_ENV !== "test") {
+    app.use(morgan("combined"));
 }
 
 app.use((req, res, next) => {
@@ -25,11 +25,11 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/', index);
-app.use('/reports', reports);
-app.use('/register', register);
-app.use('/login', login);
-app.use('/chat', chat);
+app.use("/", index);
+app.use("/reports", reports);
+app.use("/register", register);
+app.use("/login", login);
+app.use("/chat", chat);
 
 // app.post("/register", (req, res) => {
 //     const body = req.body;
@@ -56,11 +56,11 @@ app.use((err, req, res, next) => {
     }
 
     res.status(err.status || 500).json({
-        "errors": [
+        errors: [
             {
-                "status": err.status,
-                "title":  err.message,
-                "detail": err.message
+                status: err.status,
+                title: err.message,
+                detail: err.message
             }
         ]
     });
